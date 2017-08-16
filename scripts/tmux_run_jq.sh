ebbf6c04eb20d9017f01a86c34d5966c448a5f68
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2017-08-16 16:24:56 +0200

    improve error handling

--- scripts/tmux_run_jq.sh
+++ scripts/tmux_run_jq.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 buffer_content="$(tmux show-buffer)"
-status=$(curl -s -o /dev/null -w '%{http_code}' ${buffer_content})
+http_status=$(curl -s -o /dev/null -w '%{http_code}' ${buffer_content})
 
-if [ ${status} -eq 200 ]; then
-    curl -s  "$buffer_content" | jq .
-  else
-    echo "Got $status for the resource $buffer_content"
+if [ ${http_status} -eq 200 ]; then
+    json="$(curl -s $buffer_content | jq  .)"
+    if [ $? -eq 0 ]; then
+        echo "$json"
+    else
+        echo "Error: could not parse the resource of $buffer_content"
+    fi
+else
+    echo "Error: got $http_status for the resource $buffer_content"
 fi
